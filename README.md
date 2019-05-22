adc48cc5263e2f3175faaf08fffc433e89e7f492
Cynthia <cynthiamartinez@cynthias-mini.home>
2019-05-21 18:20:34 -0700

    Adding textures

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Solar System Simulator
-3D graphics programming project of a simulator where you can explore the solar system.
+Graphics project of simulator where you can explore the solar system.
 Played in the browser.
--- app.js
+++ app.js
@@ -2,31 +2,54 @@ let epX = 0;
 let epY = 0;
 let epZ = 0;
 let epR = 0;
-let rhTex;
+let earthTex;
+let marsTex;
+let sunTex;
+
 
 function setup() {
     createCanvas(windowWidth, windowHeight, WEBGL);
+    background(0,0, 0);
+    earthTex = loadImage('earth.jpg');
+    marsTex = loadImage('mars.jpg');
     
-    background(201,32, 12);
-    
-    //rhTex = loadImage("");
 }
 
 function draw() {
     
-    background(201,32, 12);
-    epR += 0.03;
-    epZ = map(mouseY, 0, height, 0, -100);
+    background(0,0, 0);
+    epX = map(mouseX, -100, height, 0, -100);
+    epY = map(mouseY, -100, height, 0, -100);
+
     renderPlanetEarth();
+    renderMarsPlanet();
+    renderSun();
     
     
 }
 
 function renderPlanetEarth() {
-    //texture(rhTex);
+
+     //epR += 0.03;
+    texture(earthTex);
     translate(epX, epY, epZ);
     rotateY(radians(epR));
-    sphere(200);
-    //code
+    sphere(200);    
 }
 
+function renderMarsPlanet() {
+    //     epR += 0;
+
+    texture(marsTex);
+    translate(400, 0, 0);
+    rotateY(radians(epR));
+    sphere(100);
+}
+
+function renderSun() {
+  //  epR += 0;
+
+    texture();
+    translate(-1000, 0, 0);
+    sphere(300);
+}
