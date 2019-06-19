242bbb86ef1b19d66aa45f072d9246b933f67530
Cynthia <cyn.mart@outlook.com>
2019-06-19 00:48:17 -0700

    Added and updated objects

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 
-
 var renderer = new THREE.WebGLRenderer();
 document.body.appendChild(renderer.domElement);
 
@@ -11,25 +10,137 @@ var controls = new THREE.OrbitControls(camera, renderer.domElement);
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 
+// Reference for geometry: (radius, segments, rings)
+//var radius = 5;  Scale
+//var segments = 50;
+//var rings = 30;
+//-------------------Planet objects---------------------
 
-var radius = 40;
-var segments = 50;
-var rings = 30;
 
-var earth_tex = new THREE.TextureLoader().load("earth1.jpg");
 
-var geometry = new THREE.SphereGeometry(radius, segments, rings);
-var material = new THREE.MeshBasicMaterial({
+// Sun
+var sun_tex = new THREE.TextureLoader().load("img/sun.png");
+var sunGeo = new THREE.SphereGeometry(90, 50, 30);
+var sunMaterial = new THREE.MeshBasicMaterial({
+ // color: 0xF3A2B0,
+  map: sun_tex
+ // wireframe: true
+});
+var sunSphere = new THREE.Mesh(sunGeo, sunMaterial);
+sunSphere.translateX(-190);
+scene.add(sunSphere);
+
+
+// Mercury
+
+var mercury_tex = new THREE.TextureLoader().load("img/mercury.jpg");
+var mercGeo = new THREE.SphereGeometry(2, 50, 30);
+var mercMaterial = new THREE.MeshBasicMaterial({ map: mercury_tex });
+var mercSphere = new THREE.Mesh(mercGeo, mercMaterial);
+mercSphere.translateX(-60);
+scene.add(mercSphere);
+
+
+// Venus
+
+var venus_tex = new THREE.TextureLoader().load("img/venus.jpg");
+var venusGeo = new THREE.SphereGeometry(4, 50, 30);
+var venusMaterial = new THREE.MeshBasicMaterial({ map: venus_tex });
+var venusSphere = new THREE.Mesh(venusGeo, venusMaterial);
+venusSphere.translateX(-30);
+scene.add(venusSphere);
+
+
+// Earth
+var earth_tex = new THREE.TextureLoader().load("img/earth.jpg");
+var earthGeo = new THREE.SphereGeometry(5, 50, 30);
+var earthMaterial = new THREE.MeshBasicMaterial({
   //color: 0xF3A2B0,
   map: earth_tex
-  //wireframe: true;
+ // wireframe: true
 });
-
-var earthSphere = new THREE.Mesh(geometry, material);
+var earthSphere = new THREE.Mesh(earthGeo, earthMaterial);
 scene.add(earthSphere);
 
-camera.position.z = 150;
+
+// Mars
+
+var mars_tex = new THREE.TextureLoader().load("img/mars1.jpg");
+var marsGeo = new THREE.SphereGeometry(4, 50, 30);
+var marsMaterial = new THREE.MeshBasicMaterial({ map: mars_tex });
+var marsSphere = new THREE.Mesh(marsGeo, marsMaterial);
+marsSphere.translateX(30);
+scene.add(marsSphere);
+
+// Jupiter
+
+var jupiter_tex = new THREE.TextureLoader().load("img/jupiter.jpg");
+var jupiterGeo = new THREE.SphereGeometry(30, 50, 30);
+var jupiterMaterial = new THREE.MeshBasicMaterial({  color: 0xF3A2B0,
+  //map: earth_tex
+  });
+var jupiterSphere = new THREE.Mesh(jupiterGeo, jupiterMaterial);
+jupiterSphere.translateX(100);
+scene.add(jupiterSphere);
+
+
+// Saturn
+
+//var saturn_tex = new THREE.TextureLoader().load("img/jupiter.jpg");
+var satGeo = new THREE.SphereGeometry(25, 50, 30);
+var satMaterial = new THREE.MeshBasicMaterial({  color: 0xF3A130,
+  //map: earth_tex
+  });
+var satSphere = new THREE.Mesh(satGeo, satMaterial);
+satSphere.translateX(200);
+scene.add(satSphere);
+
+
+
+// Uranus
+
+//var uranus_tex = new THREE.TextureLoader().load("img/jupiter.jpg");
+var urGeo = new THREE.SphereGeometry(13, 50, 30);
+var urMaterial = new THREE.MeshBasicMaterial({  color: 0xF3A2B0,
+  //map: earth_tex
+  });
+var urSphere = new THREE.Mesh(urGeo, urMaterial);
+urSphere.translateX(290);
+scene.add(urSphere);
+
+
+// Neptune
+
+//var neptune_tex = new THREE.TextureLoader().load("img/jupiter.jpg");
+var nepGeo = new THREE.SphereGeometry(12, 50, 30);
+var nepMaterial = new THREE.MeshBasicMaterial({  color: 0xF3A2B0,
+  //map: earth_tex
+  });
+var nepSphere = new THREE.Mesh(nepGeo, nepMaterial);
+nepSphere.translateX(380);
+scene.add(nepSphere);
+
+
+
+
+//--------------Skysphere-------------------
+
+var sky_tex = new THREE.TextureLoader().load("img/stars.png");
+var skyGeometry = new THREE.SphereGeometry(500, 50, 30);
+var skyMaterial = new THREE.MeshBasicMaterial({
+	map: sky_tex,
+	side: THREE.DoubleSide
+});
+var skySphere = new THREE.Mesh(skyGeometry, skyMaterial);
+scene.add(skySphere);
+
+
+//---------------Camera stuff------------------
+
+camera.position.z = 390;
 controls.update;
+
+//-------------Rendering----------------------
 
 var render = function() {
   requestAnimationFrame(render);
