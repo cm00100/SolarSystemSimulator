6f98e93e7bf53f2a9c7e4fae6b8180207f16c220
Cynthia <cyn.mart@outlook.com>
2019-06-19 03:04:18 -0700

    Resized planets smaller

--- app.js
+++ app.js
@@ -20,40 +20,40 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 
 // Sun
 var sun_tex = new THREE.TextureLoader().load("img/sun.jpg");
-var sunGeo = new THREE.SphereGeometry(90, 50, 30);
+var sunGeo = new THREE.SphereGeometry(15, 50, 30);
 var sunMaterial = new THREE.MeshBasicMaterial({
  // color: 0xF3A2B0,
   map: sun_tex
  // wireframe: true
 });
 var sunSphere = new THREE.Mesh(sunGeo, sunMaterial);
-sunSphere.translateX(-190);
+sunSphere.translateX(-40);
 scene.add(sunSphere);
 
 
 // Mercury
 
 var mercury_tex = new THREE.TextureLoader().load("img/mercury.jpg");
-var mercGeo = new THREE.SphereGeometry(2, 50, 30);
+var mercGeo = new THREE.SphereGeometry(0.2, 50, 30);
 var mercMaterial = new THREE.MeshBasicMaterial({ map: mercury_tex });
 var mercSphere = new THREE.Mesh(mercGeo, mercMaterial);
-mercSphere.translateX(-60);
+mercSphere.translateX(-10);
 scene.add(mercSphere);
 
 
 // Venus
 
 var venus_tex = new THREE.TextureLoader().load("img/venus.jpg");
-var venusGeo = new THREE.SphereGeometry(4, 50, 30);
+var venusGeo = new THREE.SphereGeometry(0.5, 50, 30);
 var venusMaterial = new THREE.MeshBasicMaterial({ map: venus_tex });
 var venusSphere = new THREE.Mesh(venusGeo, venusMaterial);
-venusSphere.translateX(-30);
+venusSphere.translateX(-5);
 scene.add(venusSphere);
 
 
 // Earth
 var earth_tex = new THREE.TextureLoader().load("img/earth1.jpg");
-var earthGeo = new THREE.SphereGeometry(5, 50, 30);
+var earthGeo = new THREE.SphereGeometry(0.5, 50, 30);
 var earthMaterial = new THREE.MeshBasicMaterial({
   //color: 0xF3A2B0,
   map: earth_tex
@@ -66,31 +66,31 @@ scene.add(earthSphere);
 // Mars
 
 var mars_tex = new THREE.TextureLoader().load("img/mars1.jpg");
-var marsGeo = new THREE.SphereGeometry(4, 50, 30);
+var marsGeo = new THREE.SphereGeometry(0.4, 50, 30);
 var marsMaterial = new THREE.MeshBasicMaterial({ map: mars_tex });
 var marsSphere = new THREE.Mesh(marsGeo, marsMaterial);
-marsSphere.translateX(30);
+marsSphere.translateX(6);
 scene.add(marsSphere);
 
 // Jupiter
 
 var jupiter_tex = new THREE.TextureLoader().load("img/jupiter.jpg");
-var jupiterGeo = new THREE.SphereGeometry(30, 50, 30);
+var jupiterGeo = new THREE.SphereGeometry(5, 50, 30);
 var jupiterMaterial = new THREE.MeshBasicMaterial({ 
   map: jupiter_tex
   });
 var jupiterSphere = new THREE.Mesh(jupiterGeo, jupiterMaterial);
-jupiterSphere.translateX(100);
+jupiterSphere.translateX(20);
 scene.add(jupiterSphere);
 
 
 // Saturn
 
 var saturn_tex = new THREE.TextureLoader().load("img/saturn.jpg");
-var satGeo = new THREE.SphereGeometry(22, 50, 30);
+var satGeo = new THREE.SphereGeometry(4, 50, 30);
 var satMaterial = new THREE.MeshBasicMaterial({ map: saturn_tex });
 var satSphere = new THREE.Mesh(satGeo, satMaterial);
-satSphere.translateX(200);
+satSphere.translateX(40);
 scene.add(satSphere);
 
 //Saturn Ring
@@ -100,27 +100,27 @@ var satRingMaterial = new THREE.MeshBasicMaterial({
   	map: saturnRing_tex
   });
 var satRing = new THREE.Mesh(satRingGeo, satRingMaterial);
-satRing.translateX(200);
+satRing.translateX(35);
 scene.add(saturnRing_tex);
 
 
 // Uranus
 
 var uranus_tex = new THREE.TextureLoader().load("img/uranus.jpg");
-var urGeo = new THREE.SphereGeometry(13, 50, 30);
+var urGeo = new THREE.SphereGeometry(2, 50, 30);
 var urMaterial = new THREE.MeshBasicMaterial({ map: uranus_tex});
 var urSphere = new THREE.Mesh(urGeo, urMaterial);
-urSphere.translateX(290);
+urSphere.translateX(56);
 scene.add(urSphere);
 
 
 // Neptune
 
 var neptune_tex = new THREE.TextureLoader().load("img/neptune.jpg");
-var nepGeo = new THREE.SphereGeometry(12, 50, 30);
+var nepGeo = new THREE.SphereGeometry(2, 50, 30);
 var nepMaterial = new THREE.MeshBasicMaterial({ map: neptune_tex });
 var nepSphere = new THREE.Mesh(nepGeo, nepMaterial);
-nepSphere.translateX(380);
+nepSphere.translateX(70);
 scene.add(nepSphere);
 
 
@@ -140,7 +140,7 @@ scene.add(skySphere);
 
 //---------------Camera stuff------------------
 
-camera.position.z = 390;
+camera.position.z = 30;
 controls.update;
 
 //-------------Rendering----------------------
